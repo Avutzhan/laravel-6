697ba79d333a916b14ad54352b841bd7c61dd057
avutzhan <dautov92@list.ru>
2020-04-25 16:38:18 +0600

    Site Template Integration

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -1,69 +1,107 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
+    <!-- meta -->
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>Laravel</title>
+    <title>Folio - Personal Portfolio Template</title>
+    <meta content="" name="keywords">
+    <meta content="" name="description">
 
-    <!-- Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,700,700i,900,900i" rel="stylesheet">
 
-    <!-- Styles -->
-    <style>
-        html, body {
-            background-color: #fff;
-            color: #636b6f;
-            font-family: 'Nunito', sans-serif;
-            font-weight: 200;
-            height: 100vh;
-            margin: 0;
-        }
+    <!-- Bootstrap CSS File -->
+    <link href="lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-        .full-height {
-            height: 100vh;
-        }
+    <!-- Libraries CSS Files -->
+    <link href="lib/ionicons/css/ionicons.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="lib/magnific-popup/magnific-popup.css" rel="stylesheet">
+    <link href="lib/hover/hover.min.css" rel="stylesheet">
 
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
+    <!-- Main Stylesheet File -->
+    <link href="css/style.css" rel="stylesheet">
 
-        .position-ref {
-            position: relative;
-        }
+    <!-- Responsive css -->
+    <link href="css/responsive.css" rel="stylesheet">
 
-        .top-right {
-            position: absolute;
-            right: 10px;
-            top: 18px;
-        }
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/favicon.png">
 
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 84px;
-        }
-
-        .links > a {
-            color: #636b6f;
-            padding: 0 25px;
-            font-size: 13px;
-            font-weight: 600;
-            letter-spacing: .1rem;
-            text-decoration: none;
-            text-transform: uppercase;
-        }
-
-        .m-b-md {
-            margin-bottom: 30px;
-        }
-    </style>
+    <!-- =======================================================
+      Theme Name: Folio
+      Theme URL: https://bootstrapmade.com/folio-bootstrap-portfolio-template/
+      Author: BootstrapMade.com
+      Author URL: https://bootstrapmade.com
+    ======================================================= -->
 </head>
+
 <body>
+    <!-- start section navbar -->
+    <nav id="main-nav">
+        <div class="row">
+            <div class="container">
+
+                <div class="logo">
+                    <a href="index.html"><img src="images/logo.png" alt="logo"></a>
+                </div>
+
+                <div class="responsive"><i data-icon="m" class="ion-navicon-round"></i></div>
+
+                <ul class="nav-menu list-unstyled">
+                    <li><a href="#header" class="smoothScroll">Home</a></li>
+                    <li><a href="/about" class="smoothScroll">About</a></li>
+                    <li><a href="#portfolio" class="smoothScroll">Portfolio</a></li>
+                    <li><a href="#journal" class="smoothScroll">Blog</a></li>
+                    <li><a href="#contact" class="smoothScroll">Contact</a></li>
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+    <!-- End section navbar -->
+
+    <!-- start section header -->
+    <div id="header" class="home">
+
+        <div class="container">
+            <div class="header-content">
+                <h1>I'm <span class="typed"></span></h1>
+                <p>designer, developeur, photographer</p>
+
+                <ul class="list-unstyled list-social">
+                    <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                    <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                    <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+                    <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                    <li><a href="#"><i class="ion-social-tumblr"></i></a></li>
+                    <li><a href="#"><i class="ion-social-dribbble"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- End section header -->
+
+
     @yield('content')
+
+<!-- JavaScript Libraries -->
+    <script src="lib/jquery/jquery.min.js"></script>
+    <script src="lib/jquery/jquery-migrate.min.js"></script>
+    <script src="lib/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/typed/typed.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="lib/magnific-popup/magnific-popup.min.js"></script>
+    <script src="lib/isotope/isotope.pkgd.min.js"></script>
+
+    <!-- Contact Form JavaScript File -->
+    <script src="contactform/contactform.js"></script>
+
+    <!-- Template Main Javascript File -->
+    <script src="js/main.js"></script>
 </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -12,3 +12,9 @@
 */
 
 Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/', function() {
+    return view('welcome');
+});
+Route::get('/about', function() {
+    return view('about');
+});
