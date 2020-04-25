cc8e34b8f68a4f1ce6f5625caae0bb487be0240d
avutzhan <dautov92@list.ru>
2020-04-25 12:28:17 +0600

    Eloquent

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,10 +10,8 @@ class PostsController extends Controller
 {
     public function show($slug)
     {
-        $post = DB::table('posts')->where('slug', $slug)->first();
-
         return view('post', [
-            'post' => $post
+            'post' => Post::where('slug', $slug)->firstOrFail()
         ]);
     }
 }
