b51b0532896401af384a6dd6a789f8c27bde72b9
avutzhan <dautov92@list.ru>
2020-04-27 22:01:53 +0600

    Webpack setting up

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -87,6 +87,32 @@
 
 
     @yield('content')
+<!-- start section footer -->
+    <div id="footer" class="text-center">
+        <div class="container">
+            <div class="socials-media text-center">
+                <ul class="list-unstyled">
+                    <li><a href="#"><i class="ion-social-facebook"></i></a></li>
+                    <li><a href="#"><i class="ion-social-twitter"></i></a></li>
+                    <li><a href="#"><i class="ion-social-instagram"></i></a></li>
+                    <li><a href="#"><i class="ion-social-googleplus"></i></a></li>
+                    <li><a href="#"><i class="ion-social-tumblr"></i></a></li>
+                    <li><a href="#"><i class="ion-social-dribbble"></i></a></li>
+                </ul>
+            </div>
+            <p>&copy; Copyrights Folio. All rights reserved.</p>
+            <div class="credits">
+                <!--
+                  All the links in the footer should remain intact.
+                  You can delete the links only if you purchased the pro version.
+                  Licensing information: https://bootstrapmade.com/license/
+                  Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Folio
+                -->
+                Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+            </div>
+        </div>
+    </div>
+    <!-- End section footer -->
 
 <!-- JavaScript Libraries -->
     <script src="lib/jquery/jquery.min.js"></script>
