24c3d9fd9d756c27c98d9ceaed8cdd4d0032de33
avutzhan <dautov92@list.ru>
2020-04-28 20:53:53 +0600

    RESTful Routes Representational State Transfer

--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,29 @@ Route::get('/about', function() {
     ]);
 });
 Route::get('/articles/{article}', 'ArticlesController@show');
+
 Route::get('/articles', 'ArticlesController@index');
+Route::get('/articles/{article}/edit', 'ArticlesController@edit');
+Route::put('/articles/{article}', 'ArticlesController@update');
+Route::delete('/articles/{article}', 'ArticlesController@delete');
+Route::post('/articles', 'ArticlesController@create');
+
+// requests GET POST PUT DELETE
+// GET /articles   index
+// GET /articles/id    show
+// POST /articles    create
+// PUT /articles/id    update
+// DELETE /articles/id    delete
+
+
+//GET /videos           index
+//GET /videos/create    create
+//POST /videos          create
+//GET /videos/2         show
+//GET /videos/2/edit    edit
+//PUT /videos/2         update
+//DELETE /videos/2/     delete
+
+//GET /videos/subscribe
+//PUT /videos/subscription create
+
