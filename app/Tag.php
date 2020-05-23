7a345100aa8fcd401a1f0fc1631e879c7153737f
Avutzhan Dautov <dautov92@list.ru>
2020-05-23 23:25:14 +0600

    Collections

--- app/Tag.php
+++ app/Tag.php
@@ -40,4 +40,20 @@ class Tag extends Model
 //to restrict this we must add to migration
 //$table->primary(['post_id', 'tag_id']);
 //onDelete('cascade'); delete other by cascade if deleted first model
-
+//in tinker
+//$tags->first(function ($tag) { return strlen($tag->name) > 5; });
+//collect(['one','two','three']);
+//collect(['one','two','three'])->first();
+//collect(['one','two','three', ['rrr', 'rrr']])->flatten();
+//$items = collect([1,2,3,4,5,6,7]);
+//$items->filter(function ($item) { return $item >= 5; });
+//$filtered = $items->filter(function ($item) { return $item >= 5; });
+//$items->filter(function ($item) { return $item % 2 === 0; });
+//$items->filter(function ($item) { return $item % 2 === 0; })->map(function ($item) { return $item + 5; });
+//$articles->pluck('tags')->collapse()->pluck('name');
+// тема урока в том что мы можем манипулировать окллекциями как нам удобно создавая цепочки из методово
+// Пора переходить на уровень выше с использованием кода нужно вместо самописных велосипедов переходить на функции высшего порядка и строенные методы языка и фреймворка нужна практика пес повтори фрикодкамп
+//$test->unique();
+// $articles->pluck('tags.*.name'); выводим данные со связи тагс у артикля
+//collect(['one', 'one', 'two', 'two'])->unique()->map(function ($item) { return ucwords($item); });
+//ucwords upper case words
