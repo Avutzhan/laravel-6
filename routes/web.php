cdad7dae73e6ca3684b50d5eef12d6ba2afdf45e
Avutzhan Dautov <dautov92@list.ru>
2021-03-31 09:43:17 +0600

    add contact to crm with vanilla php

--- routes/web.php
+++ routes/web.php
@@ -138,3 +138,76 @@ Route::get('/home', 'HomeController@index')
 //john => moderator, sally => manager, frank => owner
 //moderator => edit form,
 //owner => view financial reports
+
+Route::get('/add-contact-form', function() {
+    return view('add-contact-form');
+});
+
+Route::post('/add-contact', function() {
+    $method = "crm.contact.add";
+
+    $name 		    = isset($_POST['name']) ? $_POST['name'] : '';
+    $phone 		    = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $description    = isset($_POST['description']) ? $_POST['description'] : '';
+    $email 			= isset($_POST['email']) ? $_POST['email'] : '';
+    $company        = isset($_POST['company']) ? $_POST['company'] : '';
+
+    $contact = array(
+        'NAME' => $name,
+        'PHONE' => $phone,
+        'DESCRIPTION' => $description,
+        'EMAIL' => $email,
+        'COMPANY' => $company,
+        'CONTACT_ID' => 0,
+        'COMPANY_ID' => 0,
+        'DEAL_ID' => 0,
+    );
+
+    function sendDataToBitrix($method, $data) {
+        $queryUrl = "https://b24-nnei4t.bitrix24.ru/rest/1/c34a1uh095ffg400/" . $method ;
+        $queryData = http_build_query($data);
+
+        $curl = curl_init();
+        curl_setopt_array($curl, array(
+            CURLOPT_SSL_VERIFYPEER => 0,
+            CURLOPT_POST => 1,
+            CURLOPT_HEADER => 0,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_URL => $queryUrl,
+            CURLOPT_POSTFIELDS => $queryData,
+        ));
+
+        $result = curl_exec($curl);
+        curl_close($curl);
+        return json_decode($result, 1);
+    }
+
+    function addContact($contact) {
+        $check = checkContact($contact);
+        if($check['total'] != 0) return $check['result'][0]['ID'];
+        $contactData = sendDataToBitrix('crm.contact.add', [
+            'fields' => [
+                'NAME' => $contact['NAME'],
+                'EMAIL' => [['VALUE' => $contact['EMAIL'], 'VALUE_TYPE' => 'WORK']],
+                'PHONE' => [['VALUE' => $contact['PHONE'], 'VALUE_TYPE' => 'WORK']],
+                'TYPE_ID' => 'CLIENT',
+                'COMPANY_ID' => $contact['COMPANY_ID'],
+            ], 'params' => [
+                'REGISTER_SONET_EVENT' => 'Y'
+            ]
+        ]);
+
+        return $contactData['result'];
+    }
+
+    function checkContact($contact){
+        $list = sendDataToBitrix('crm.contact.list', [
+            'filter' => [ 'PHONE' =>  $contact['PHONE']],
+            'select' => [ 'ID'],
+        ]);
+        return $list;
+    }
+
+    addContact($contact);
+
+})->name('crm.add.contact');
